4cb1c07de7dad72ab9e918d15de89a7efa7e4b2e
nemoskii <88425419+nemoskii@users.noreply.github.com>
2021-11-01 01:42:54 +0900

    Update UserMapper.java

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -1,6 +1,5 @@
 package com.example.repository;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -17,16 +16,5 @@ public interface UserMapper {
     //select文の実行結果が複数件になる場合、メソッドの戻り値をListにする
     //**ユーザー取得*/
     public List<MUser> findMany();
-=======
-import org.apache.ibatis.annotations.Mapper;
-
-import com.example.domain.user.model.MUser;
-
-@Mapper
-public interface UserMapper {
-    //インターフェースに＠MapperアノテーションをつけることでMyBatisでリポジトリー作成できる
-    /**ユーザー登録*/
-    public int insertOne(MUser user);
->>>>>>> refs/heads/master
 
 }
