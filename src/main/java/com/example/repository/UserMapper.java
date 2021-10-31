a971ffc0b250f6754b3d79e104672ab40a180458
nemoskii <sk.nmt8@gmail.com>
2021-11-01 00:59:08 +0900

    select 複数件

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -1,13 +1,20 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.domain.user.model.MUser;
+
 
 @Mapper
 public interface UserMapper {
     //インターフェースに＠MapperアノテーションをつけることでMyBatisでリポジトリー作成できる
     /**ユーザー登録*/
     public int insertOne(MUser user);
+    
+    //select文の実行結果が複数件になる場合、メソッドの戻り値をListにする
+    //**ユーザー取得*/
+    public List<MUser> findMany();
 
 }
